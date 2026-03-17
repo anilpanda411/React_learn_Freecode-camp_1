bba774d2393476710c5422575afc467ad842e339
Anil Panda <anil.learn.programming@gmail.com>
2026-03-17 21:06:26 +0530

    tuesday push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => <img src="https://m.media-amazon.com/images/I/71PjIDe6FLL._SY466_.jpg" alt="Theo of Golden: A Novel"/>;
+const Title = () => {
+  return <h2>Theo of Golden: A Novel </h2>;
+};
+const Author = () => <h4 style={{ color: '#16324c', fontSize: '1rem', marginTop: '0.5rem' }}>Allen Levi</h4>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);
